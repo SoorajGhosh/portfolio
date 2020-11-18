eef4d3739e3e69496df403011f32fbf1b44d0e1f
SoorajGhosh <soorajghosh78@gmail.com>
2020-11-19 04:30:04 +0530

    Navbar design finished !

---
+++ main.js
@@ -0,0 +1,102 @@
+let mobileView, menu=true;
+
+
+let uiElements = (function(){
+
+    let dom ={
+        menuBtn : document.querySelector('.menu-btn'),
+        inner : document.querySelector('.inner'),
+        nav : document.querySelector('.nav'),
+        navLinks : Array.from(document.querySelectorAll('.nav-links')),
+        navUl : document.querySelector('.nav-ul'),
+    }
+
+    //The inner element slides down
+    const innerDive = function(){
+        if (menu){
+            dom.inner.style.top='100vh';
+            dom.navLinks.map((cur)=>cur.classList.add('nav-links-show'))
+            dom.navUl.classList.add('nav-links-center');
+            menu=false;
+        } else {
+            dom.inner.style.top='53px';
+            dom.navLinks.map((cur)=>cur.classList.remove('nav-links-show'))
+            dom.navUl.classList.remove('nav-links-center');
+            menu=true;
+        }
+    }
+
+    return {dom,innerDive}
+})(); 
+
+
+
+let controller = (function(){
+    const dom = uiElements.dom;
+
+    function test(){console.log('Cleared');}
+    
+    // Event Listeners
+    dom.menuBtn.addEventListener('click', uiElements.innerDive);
+    
+})();
+
+
+
+
+
+(function responsive(){
+    let media = window.matchMedia("(max-width: 600px)")
+
+    let checkMedia = mediaQuery => {
+        mobileView = mediaQuery.matches;
+        return mobileView;
+    }
+
+    checkMedia(media);
+
+    media.addListener(checkMedia)
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
