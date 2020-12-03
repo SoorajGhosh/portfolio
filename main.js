3534506dd7d11d2f403a05ee660d9bc458486ec4
SoorajGhosh <soorajghosh78@gmail.com>
2020-12-04 04:18:01 +0530

    WIP: About Carousal.

--- main.js
+++ main.js
@@ -9,7 +9,12 @@ let uiElements = (function(){
         nav : document.querySelector('.nav'),
         navLinks : Array.from(document.querySelectorAll('.nav-links')),
         navUl : document.querySelector('.nav-ul'),
-        targetBtns : document.querySelectorAll('.target-btn')
+        targetBtns : document.querySelectorAll('.target-btn'),
+        aboutAnchors:  Array.from(document.querySelectorAll('.about-anchor')),
+        aboutWrap: document.querySelector('.about-wrap'),
+        aboutContainer: document.querySelector('.about-container'),
+        aboutSection: document.querySelector('.about'),
+        aboutCards: Array.from(document.querySelectorAll('.about-card')),
     }
 
     // Showing/Removing the NAV
@@ -32,20 +37,45 @@ let controller = (function(){
     
     // Event Listeners
     dom.menuBtn.addEventListener('click', uiElements.showNav);
+    
+    // NAVBAR
     dom.nav.addEventListener('click', function(e){
         e.preventDefault();
-        const section = e.target.closest('.nav-target');
-        if (!section) return;
-        uiElements.showNav();
-        const sectionID = section.getAttribute('href')
-        document.querySelector(sectionID).scrollIntoView({behavior:'smooth'});
+        const section_id = e.target.closest('.nav-target')?.getAttribute('href');
+        if (!section_id) return;
+        (section_id!=='#intro-section') ? uiElements.showNav():undefined;
+        document.querySelector(section_id).scrollIntoView({behavior:'smooth'});
     });
+
+    // In between links to go somewhere on the page
     dom.targetBtns.forEach((cur)=>cur.addEventListener('click', function(e){
         e.preventDefault();
         const sectionID = e.target.getAttribute('href')
         document.querySelector(sectionID).scrollIntoView({behavior:'smooth'});
-    }))
+    }));
 
+    //=============ABOUT===========================
+    dom.aboutSection.addEventListener('click', function(e){
+        e.preventDefault();
+        const active_card_id = e.target.closest('.about-anchor')?.getAttribute('href');
+        if (!active_card_id) return;
+        console.log(active_card_id);
+        document.querySelector(active_card_id).scrollIntoView({behavior:'smooth'});
+    })
+
+    // dom.aboutContainer.addEventListener('scroll',function(){
+    //     console.log('scrolled');
+    // })
+
+    //creating clones of ABout container
+    let nextAboutContainer=[], prevAboutContainer=[];
+    dom.aboutCards.forEach((cur,i,arr)=>{
+        nextAboutContainer.push(dom.aboutCards[i].cloneNode(true));
+        prevAboutContainer.unshift(dom.aboutCards[i].cloneNode(true));
+    })
+    // console.log(prevAboutContainer, nextAboutContainer);
+    
+    
 })();
 
 // Setting the Page height according to the phone's window in responsive. Solution SOURCE: https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
